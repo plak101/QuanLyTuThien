2c921a24e5400b73dbf82d60c8ce54dae30cdcec
plak101 <phamleanhkhoa2005@gmail.com>
2025-03-26 14:24:21 +0700

    add properties

--- src/dao/ICharityEventDAO.java
+++ src/dao/ICharityEventDAO.java
@@ -1,13 +1,20 @@
 
 package dao;
 
-import model.CharityEvent;
+import entity.CharityEvent;
 import java.util.List;
-
+import java.sql.*;
+        
 /**
  *
  * @author phaml
  */
 public interface ICharityEventDAO {
     public List<CharityEvent> getEventList();
+    public boolean addEvent(CharityEvent event);
+    public boolean updateEvent(CharityEvent event);
+    
+    //Đóng kết nối
+
+    
 }

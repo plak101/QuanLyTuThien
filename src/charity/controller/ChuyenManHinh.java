b0b310978adebc280d93c1d548bc58bb4814368b
taplamcode <tranthanhngan.29.05.2005@gmail.com>
2025-04-03 11:15:46 +0700

    gopcode

---
+++ src/charity/controller/ChuyenManHinh.java
@@ -0,0 +1,128 @@
+package charity.controller;
+
+import charity.bean.DanhMuc;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.List;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import charity.viewMain.QuanLyQuyenGop;
+import charity.viewMain.QuanLyChuongTrinh;
+import charity.viewMain.BaoCao;
+import charity.viewMain.QuanLyNhaTaiTro;
+import charity.viewMain.TrangChu;
+
+public class ChuyenManHinh {
+
+    private final JPanel root;
+    private String kindSelected = "";
+    private List<DanhMuc> listItem;
+
+    public ChuyenManHinh(JPanel jpnRoot) {
+        this.root = jpnRoot;
+    }
+
+    public void setView(JPanel jpnItem, JLabel jibItem) {
+        kindSelected = "TrangChu";
+        jpnItem.setBackground(new Color(96, 100, 191));
+        jibItem.setBackground(new Color(96, 100, 191));
+
+        root.removeAll();
+        root.setLayout(new BorderLayout());
+        root.add(new TrangChu());
+        root.validate();
+        root.repaint();
+    }
+
+    public void setEvent(List<DanhMuc> listItem) {
+        this.listItem = listItem;
+        for (DanhMuc item : listItem) {
+            item.getJib().addMouseListener(new LabelEvent(
+                    item.getKind(), item.getJpn(), item.getJib()));
+        }
+    }
+
+    class LabelEvent implements MouseListener {
+
+        private JPanel node;
+
+        private String kind;
+        private JPanel jpnItem;
+        private JLabel jpbItem;
+
+        public LabelEvent(String kind, JPanel jpnItem, JLabel jpbItem) {
+            this.kind = kind;
+            this.jpnItem = jpnItem;
+            this.jpbItem = jpbItem;
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            switch (kind) {
+                case "TrangChu":
+                    node = new TrangChu();
+                    break;
+                case "QuanLyNhaTaiTro":
+                    node = new QuanLyNhaTaiTro();
+                    break;
+                case "QuanLyChuongTrinh":
+                    node = new QuanLyChuongTrinh();
+                    break;
+                case "QuanLyQuyenGop":
+                    node = new QuanLyQuyenGop();
+                    break;
+                case "BaoCao":
+                    node = new BaoCao();
+                    break;
+                default:
+                    node = new TrangChu();
+                    break;
+            }
+            root.removeAll();
+            root.setLayout(new BorderLayout());
+            root.add(node);
+            root.validate();
+            root.repaint();
+            setChangeBackground(kind);
+            kindSelected = kind;
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            kindSelected = kind;
+            jpnItem.setBackground(new Color(96, 100, 191));
+            jpbItem.setBackground(new Color(96, 100, 191));
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            jpnItem.setBackground(new Color(96, 100, 191));
+            jpbItem.setBackground(new Color(96, 100, 191));
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            if (!kindSelected.equalsIgnoreCase(kind)) {
+                jpnItem.setBackground(new Color(76, 175, 80));
+                jpbItem.setBackground(new Color(76, 175, 80));
+            }
+        }
+        public void setChangeBackground(String kind){
+            for(DanhMuc item : listItem){
+                if(item.getKind().equalsIgnoreCase(kind)){
+                    item.getJpn().setBackground(new Color(96,100,191));
+                    item.getJib().setBackground(new Color(96,100,191));
+                }else{ 
+                    item.getJpn().setBackground(new Color(76,175,80));
+                    item.getJib().setBackground(new Color(76,175,80));
+                } 
+            }
+        }
+    }
+}
---
+++ src/charity/viewMain/GradientPanel.java
@@ -0,0 +1,50 @@
+package charity.viewMain;
+
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+/**
+ *
+ * @author phaml
+ */
+public class GradientPanel extends javax.swing.JPanel {
+
+    public GradientPanel() {
+        initComponents();
+//        GradientPaint g = new GradientPaint(0, 0, Color.decode("#758EB7"), 0, getHeight(), Color.decode("#A5CAD2"));
+        new GradientPaint(0, 0, Color.decode("#141E30"), 0, getHeight(), Color.decode("#243B55"));
+    }
+    @Override
+    protected void paintChildren(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        GradientPaint gradientPaint = new GradientPaint(0, 0, Color.decode("#2c6975"), 0, getHeight(), Color.decode("#68b2a0"));
+        g2d.setPaint(gradientPaint);
+        g2d.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
+        super.paintChildren(g); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+
+
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 142, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 342, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}

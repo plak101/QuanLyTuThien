f88a8d66e11e265253556e2d77a52a9487a18397
plak101 <phamleanhkhoa2005@gmail.com>
2025-04-27 09:52:03 +0700

    final2 UserUi

--- src/charity/view/QLCTTT.java
+++ src/charity/view/QLCTTT.java
@@ -4,6 +4,6 @@ package charity.view;
 import charity.viewMain.AdminUI;
 public class QLCTTT {
     public static void main(String[] args) {
-      new AdminUI(1).setVisible(true);
+//      new AdminUI(1).setVisible(true);
     }
 }
